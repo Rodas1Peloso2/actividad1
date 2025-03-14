1924934cb28b6df1270210f21b16b3adb1f37105
Rodas1Peloso2 <peloso.rodas@gmail.com>
2025-03-14 13:27:58 -0300

    Add files via upload

---
+++ repaso.py
@@ -0,0 +1,6 @@
+bucle for
+numeros=[1,2,3,20,4,5]
+for numero in numeros:
+    cuadrado=numero**2
+    print(f"El cuadrado de {numero} es {cuadrado}")
+print (numeros [5])
---
+++ repaso2.py
@@ -0,0 +1,18 @@
+nombre="marco"
+apellido="MENDOZA"
+texto="Hola Esto Es Un Texto"
+longitud=len(texto)
+print(longitud)
+print(apellido[5])
+palabra=texto.split()
+print(palabra)
+mayusculas=texto.upper()
+print (mayusculas)
+minusculas=texto.lower()
+print(minusculas)
+
+lista=[1,2,3,4,5]
+lista.append(8)
+print(lista)
+del lista[1]
+print(lista)
